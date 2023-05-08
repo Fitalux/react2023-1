049682aeb2d2efa02bac3b369dbd426264f81429
Fitalux <goed0522@gmail.com>
2023-05-08 12:59:25 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function clock() {
+//     let clock = document.getElementById("clock");
+// }
+
+// setInterval(function(){
+//     clock.innerHTML = new Date().toLocaleDateString();
+// }, 1000);
+// clock();
+
+
+function App(){
+    return(
+    <div>
+        <h1>hello react</h1>
     </div>
-  );
+    )
 }
 
 export default App;
